a02f4a5342878a5072532db8b9b56f778a062f4e
John Sticher <john.sticher@gmail.com>
2023-05-29 16:49:05 -0400

    initial commit

---
+++ api/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from rest_framework import generics
+from .models import Item, Location
+from .serializers import ItemSerializer, LocationSerializer
+
+# Create your views here.
+def homePageView(request):
+    return HttpResponse("John Sticher Rules!")
+
+def apiView(request):
+    return HttpResponse("Get Your API's Here!!!")
+
+class ItemList(generics.ListCreateAPIView):
+    serializer_class = ItemSerializer
+
+    def get_queryset(self):
+        queryset = Item.objects.all()
+        location = self.request.query_params.get('location')
+        if location is not None:
+            queryset = queryset.filter(itemLocation=location)
+        return queryset
+    
+class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ItemSerializer
+    queryset = Item.objects.all()
+
+
+class LocationList(generics.ListCreateAPIView):
+    serializer_class = LocationSerializer
+    queryset = Location.objects.all()
+
+
+class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = LocationSerializer
+    queryset = Location.objects.all()
+
