dc940433d1047c176cd2bbfd650422b793ef65c6
John Sticher <john.sticher@gmail.com>
2023-05-29 22:12:10 -0400

    removed secret key

--- storage/settings.py
+++ storage/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1^-6+2^7qzl8=x*+9jre*@&21p0!hb!voaj=62---n9e8ghydm'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
